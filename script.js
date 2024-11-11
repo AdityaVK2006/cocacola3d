4ba1ac2d0db808045b862a2874817a09d863a279
AdityaVK2006 <adityakatepallewar@gmail.com>
2024-11-11 14:20:42 +0530

    modified

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
@@ -11,7 +11,11 @@ const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.querySelector('.main').appendChild(renderer.domElement);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+controls.enableZoom = false;
+controls.enablePan = false;
+controls.enableRotate = true;
 
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene, camera);
@@ -19,23 +23,26 @@ composer.addPass(renderPass);
 
 const bloomPass = new UnrealBloomPass(
     new THREE.Vector2(window.innerWidth, window.innerHeight),
-    1.7, 
-    1.7, 
+    1.5, 
+    1, 
     0.1 
 );
 composer.addPass(bloomPass);
 
 const texture = new THREE.TextureLoader().load('tex.jpg');
 const geometry = new THREE.CylinderGeometry(1, 1, 3, 32);
-const material = new THREE.MeshBasicMaterial({map: texture});
-const can = new THREE.Mesh(geometry, material);
+const materials = [
+    new THREE.MeshBasicMaterial({map: texture}), 
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('top.jpg')}), 
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('bottom.webp')}) 
+];
+const can = new THREE.Mesh(geometry, materials);
 can.rotation.y = Math.PI / 2;
 scene.add(can);
 
 camera.position.z = 5;
 
 gsap.registerPlugin(ScrollTrigger);
-
 
 gsap.to(document.querySelector('.main h1'), {
     x: 1500,
@@ -46,14 +53,17 @@ gsap.to(document.querySelector('.main h1'), {
         scrub: 2,
         pin: '.main',
     }
-})
+});
 
-document.querySelector('.main').addEventListener('mousemove', (e) => {
-    gsap.to(can.rotation, {
-        y: (e.clientX/window.innerWidth)*Math.PI,
-        duration: 0.7,
-    })
-})
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    composer.setSize(window.innerWidth, window.innerHeight);
+    bloomPass.resolution.set(window.innerWidth, window.innerHeight);
+}
 
 function animate() {
     requestAnimationFrame(animate);
